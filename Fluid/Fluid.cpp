c1a859205468576363d412bdbd05934272b7ca80
Charl-Pierre Marais <charlpierre0301@gmail.com>
2024-06-14 16:29:08 +0200

    sorta got opencl doing smth, idk

--- Fluid/Fluid.cpp
+++ Fluid/Fluid.cpp
@@ -1,8 +1,9 @@
 #include "Fluid.h"
 #include <cmath>	// std::floor
-#include <CL/cl.h>
-
-#define MAX_SOURCE_SIZE (0x100000)
+#include <CL/cl.hpp>
+#include <fstream>
+#include <iostream>
+using namespace std;
 
 Fluid::Fluid()
 	:
@@ -36,107 +37,50 @@ Fluid::Fluid()
 
 void Fluid::Update() noexcept
 {
-	// Create the two input vectors
-	int i;
-	const int LIST_SIZE = 1024;
-	int* A = (int*)malloc(sizeof(int) * LIST_SIZE);
-	int* B = (int*)malloc(sizeof(int) * LIST_SIZE);
-	for (i = 0; i < LIST_SIZE; i++) {
-		A[i] = i;
-		B[i] = LIST_SIZE - i;
+
+	//Test
+	std::vector<cl::Platform> platforms;
+	cl::Platform::get(&platforms);
+
+	auto platform = platforms.front();
+	std::vector<cl::Device> devices;
+	platform.getDevices(CL_DEVICE_TYPE_GPU, &devices); //maybe change to TYPE_ALL if necessary
+
+	auto device = devices.front();
+
+	std::ifstream helloWorldFile("program.cl");
+	std::string src(std::istreambuf_iterator<char>(helloWorldFile), (std::istreambuf_iterator<char>()));
+
+	cl::Program::Sources sources(1, std::make_pair(src.c_str(), src.length() + 1));
+	
+	cl::Context context(device);
+	cl::Program program(context, sources);
+
+	auto err = program.build("-cl-std=CL1.2");
+
+	char buf[16];
+	int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8};
+
+	cl::Buffer memBuf(context, CL_MEM_READ_WRITE, sizeof(buf)); //Change read/write rights if necessary
+	cl::Buffer memNum(context, CL_MEM_READ_WRITE, sizeof(numbers), numbers); //Change read/write rights if necessary
+	cl::Kernel kernel(program, "HelloWorld", &err);
+	kernel.setArg(0, memBuf);
+	kernel.setArg(1, memNum);
+
+	cl::CommandQueue queue(context, device);
+	queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(8));
+	queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, sizeof(buf), buf);
+	queue.enqueueReadBuffer(memNum, CL_TRUE, 0, sizeof(numbers), numbers);
+
+	int length = sizeof(numbers) / sizeof(int);
+	for (int i = 0; i < length; i++)
+	{
+		cout << numbers[i] << endl;
 	}
+	cout << buf << endl;
+	std::cin.get();
 
-	// Load the kernel source code into the array source_str
-	FILE* fp;
-	char* source_str;
-	size_t source_size;
-#pragma warning (disable : 4996)
-	fp = fopen("program.cl", "r");
-	if (!fp) {
-		fprintf(stderr, "Failed to load kernel.\n");
-		exit(1);
-	}
-	source_str = (char*)malloc(MAX_SOURCE_SIZE);
-	source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
-	fclose(fp);
-
-	// Get platform and device information
-	cl_platform_id platform_id = NULL;
-	cl_device_id device_id = NULL;
-	cl_uint ret_num_devices;
-	cl_uint ret_num_platforms;
-	cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1,
-		&device_id, &ret_num_devices);
-
-	// Create an OpenCL context
-	cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
-
-	// Create a command queue
-	cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
-
-	// Create memory buffers on the device for each vector 
-	cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-		LIST_SIZE * sizeof(int), NULL, &ret);
-	cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-		LIST_SIZE * sizeof(int), NULL, &ret);
-	cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-		LIST_SIZE * sizeof(int), NULL, &ret);
-
-	// Copy the lists A and B to their respective memory buffers
-	ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
-		LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
-	ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0,
-		LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
-
-	// Create a program from the kernel source
-	cl_program program = clCreateProgramWithSource(context, 1,
-		(const char**)&source_str, (const size_t*)&source_size, &ret);
-
-	int x = 1;
-
-	// Build the program
-	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
-
-	int y = 2;
-
-	// Create the OpenCL kernel
-	cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
-
-	// Set the arguments of the kernel
-	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&a_mem_obj);
-	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&b_mem_obj);
-	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&c_mem_obj);
-
-	// Execute the OpenCL kernel on the list
-	size_t global_item_size = LIST_SIZE; // Process the entire lists
-	size_t local_item_size = 64; // Divide work items into groups of 64
-	ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL,
-		&global_item_size, &local_item_size, 0, NULL, NULL);
-
-	// Read the memory buffer C on the device to the local variable C
-	int* C = (int*)malloc(sizeof(int) * LIST_SIZE);
-	ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0,
-		LIST_SIZE * sizeof(int), C, 0, NULL, NULL);
-
-	// Display the result to the screen
-	for (i = 0; i < LIST_SIZE; i++)
-		printf("%d + %d = %d\n", A[i], B[i], C[i]);
-
-	// Clean up
-	ret = clFlush(command_queue);
-	ret = clFinish(command_queue);
-	ret = clReleaseKernel(kernel);
-	ret = clReleaseProgram(program);
-	ret = clReleaseMemObject(a_mem_obj);
-	ret = clReleaseMemObject(b_mem_obj);
-	ret = clReleaseMemObject(c_mem_obj);
-	ret = clReleaseCommandQueue(command_queue);
-	ret = clReleaseContext(context);
-	free(A);
-	free(B);
-	free(C);
-	//return 0;
+	//End test
 
 	Diffuse(1, Vx0.data(), Vx.data(), VISCOSITY, MOTION_SPEED);
 	Diffuse(2, Vy0.data(), Vy.data(), VISCOSITY, MOTION_SPEED);
--- program.cl
+++ program.cl
@@ -1,10 +1,19 @@
-#include "cl/program.h"
+__kernel void HelloWorld(__global char* data, __global int* number)
+{
+data[0] = 'H';
+data[1] = 'e';
+data[2] = 'l';
+data[3] = 'l';
+data[4] = 'o';
+data[5] = 'W';
+data[6] = 'o';
+data[7] = 'r';
+data[8] = 'l';
+data[9] = 'd';
+data[10] = '\n';
 
-__kernel void vector_add(__global const int *A, __global const int *B, __global int *C) {
- 
-    // Get the index of the current element to be processed
-    int i = get_global_id(0);
- 
-    // Do the operation
-    C[i] = A[i] + B[i];
+int i = get_global_id(0);
+int num = number[i];
+number[i] = num + 3;
+
 }
